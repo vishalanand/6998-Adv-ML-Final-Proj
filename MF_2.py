8564075fc7a5dfdc1770c21b9f1ebb4937e462ba
Kirill Sydykov <sydykov@users.noreply.github.com>
2018-05-07 13:08:48 -0400

    Improved style.
    Configured to work on artists data.

--- MF_2.py
+++ MF_2.py
@@ -18,37 +18,39 @@ from sklearn import decomposition
 
 # In[2]:
 
+PREFIX = 'artist_'
+IID = 'iid' # item ID
+PID = 'pid' # playlist ID
 
-# DATA_DIR = 'mpd_proc/data_small_2'
-DATA_DIR = 'mpd_proc/data_large'
+DATA_DIR = 'mpd_proc/data_2'
 
-unique_sid = list()
-with open(os.path.join(DATA_DIR, 'unique_sid.txt'), 'r') as f:
+unique_iid = list()
+with open(os.path.join(DATA_DIR, PREFIX + 'unique_' + PREFIX[0] + IID[1:] + '.txt'), 'r') as f:
     for line in f:
-        unique_sid.append(line.strip())
+        unique_iid.append(line.strip())
 
-n_items = len(unique_sid)
+n_items = len(unique_iid)
 
 
 # In[3]:
 
 
-df = pd.read_csv(os.path.join(DATA_DIR, 'train.csv'))
-n_users = df['uid'].max() + 1
+df = pd.read_csv(os.path.join(DATA_DIR, PREFIX + 'train.csv'))
+n_playlists = df[PID].max() + 1
 
-rows, cols = df['uid'], df['sid']
+rows, cols = df[PID], df[IID]
 train_data = sparse.csr_matrix((np.ones_like(rows), (rows, cols)),
                                dtype='float64',
-                               shape=(n_users, n_items))
+                               shape=(n_playlists, n_items))
 
 
 # In[4]:
 
 
-sparsity = 1. * df.shape[0] / (n_users * n_items)
+sparsity = 1. * df.shape[0] / (n_playlists * n_items)
 
-print("%d playlist inclusion events from %d playlist and %d tracks (sparsity: %.3f%%)" % 
-      (df.shape[0], n_users, n_items, sparsity * 100))
+print("%d playlist inclusion events from %d playlist and %d %s (sparsity: %.3f%%)" %
+      (df.shape[0], n_playlists, n_items, PREFIX[:-1] + 's', sparsity * 100))
 
 
 # In[5]:
@@ -78,11 +80,11 @@ def load_tr_te_data(csv_file_tr, csv_file_te):
     tp_tr = pd.read_csv(csv_file_tr)
     tp_te = pd.read_csv(csv_file_te)
 
-    start_idx = min(tp_tr['uid'].min(), tp_te['uid'].min())
-    end_idx = max(tp_tr['uid'].max(), tp_te['uid'].max())
+    start_idx = min(tp_tr[PID].min(), tp_te[PID].min())
+    end_idx = max(tp_tr[PID].max(), tp_te[PID].max())
 
-    rows_tr, cols_tr = tp_tr['uid'] - start_idx, tp_tr['sid']
-    rows_te, cols_te = tp_te['uid'] - start_idx, tp_te['sid']
+    rows_tr, cols_tr = tp_tr[PID] - start_idx, tp_tr[IID]
+    rows_te, cols_te = tp_te[PID] - start_idx, tp_te[IID]
 
     data_tr = sparse.csr_matrix((np.ones_like(rows_tr), (rows_tr, cols_tr)),
                                 dtype='float64',
@@ -96,8 +98,8 @@ def load_tr_te_data(csv_file_tr, csv_file_te):
 # In[9]:
 
 
-valid_train, valid_test = load_tr_te_data(os.path.join(DATA_DIR, 'validation_tr.csv'),
-                                          os.path.join(DATA_DIR, 'validation_te.csv'))
+valid_train, valid_test = load_tr_te_data(os.path.join(DATA_DIR, PREFIX + 'validation_tr.csv'),
+                                          os.path.join(DATA_DIR, PREFIX + 'validation_te.csv'))
 
 
 # In[10]:
@@ -193,21 +195,31 @@ class MF():
         if self.trained:
             most_similar_rows = self.find_most_similar_rows(X)
             return self.W[most_similar_rows].dot(self.H.T)
-        
+            # predictions = np.array([], dtype=np.float64)
+            # for row in range(X.shape[0]):
+            #     features = X[row].toarray()[0]
+            #     prediction = self._predict_single(features)
+            #     if predictions.size == 0:
+            #         predictions = prediction
+            #     else:
+            #         predictions = np.vstack((predictions, prediction))
+            # return predictions
+
     def find_most_similar_rows(self, X):
         most_similar_rows = np.array([], dtype=np.int64)
         for row in range(X.shape[0]):
             features = X[row].toarray()[0]
             most_similar_row = self.find_most_similar_row(features)
-#             if most_similar_rows.size == 0:
-#                 most_similar_rows = most_similar_row
-#             else:
-#                 most_similar_rows = np.vstack((most_similar_rows, most_similar_row))
             most_similar_rows = np.append(most_similar_rows, most_similar_row)
         return most_similar_rows
     
     def find_most_similar_row(self, features):
         return np.argmax(self.data.dot(features.T))
+
+    def _predict_single(self, features):
+        playlist_similarity_matrix = self.data.dot(features.T)
+        most_similar_playlists = np.argwhere(playlist_similarity_matrix == np.max(playlist_similarity_matrix))
+        return np.sum(self.W[most_similar_playlists], axis=0).dot(self.H.T)
 
 
 # In[16]:
@@ -250,9 +262,9 @@ def calculate_metrics(model, val_train_data, val_test_data, k=100, logs={}):
 # In[22]:
 
 
-num_factors_list = [350]
-num_iterations_list = [10]
-lambda_reg_list = [1e-3] #, 1e-1, 1e1, 1e2]
+num_factors_list = [75]
+num_iterations_list = [5]
+lambda_reg_list = [1e-5] #, 1e-1, 1e1, 1e2]
 linear_surplus_alpha_list = [5]
 
 
